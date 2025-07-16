3ef57ffea6e1dbe10fc2510a971b35cfd5d5fe6e
fomn <felipe.neves16@gmail.com>
2025-07-15 21:04:08 -0400

    testing openvoice_v2

--- ml/models/xtts/xtts_main.py
+++ ml/models/xtts/xtts_main.py
@@ -2,29 +2,34 @@ from pathlib import Path
 import scipy
 import numpy as np
 import time
-from cusom_implementation.vc.models.freevc import FreeVC
-from cusom_implementation.api import TTS
+from TTS.vc.models.openvoice import OpenVoice
+from TTS.api import TTS
+import os
 
 def save_to_wav(wav):
+    path = "testOutputs/"
+    os.makedirs(path, exist_ok=True)
+    path +=f"wav{time.time()}.wav"
+
     wav_norm = wav * (32767 / max(0.01, np.max(np.abs(wav))))
     wav_norm = wav_norm.astype(np.int16)
-    scipy.io.wavfile.write(f"testOutputs/wav{time.time()}.wav", vc_model.config.audio.output_sample_rate, wav_norm)
+    scipy.io.wavfile.write(path, vc_model.config.audio.output_sample_rate, wav_norm)
 
 print("Downloading model...")
-tts = TTS(model_name="voice_conversion_models/multilingual/vctk/freevc24", progress_bar=False).cuda()
-vc_model: FreeVC = tts.voice_converter.vc_model # type: ignore
+tts = TTS(model_name="voice_conversion_models/multilingual/multi-dataset/openvoice_v2").cuda()
+vc_model: OpenVoice = tts.voice_converter.vc_model
 
-print("Computing speaker latents...")
-speaker_embedding = vc_model.get_speaker_embedding("voice-profile.wav")
+print("Computing and caching speaker latents...")
+vc_model.voice_conversion("source_voice.wav","source_voice.wav", speaker_id="MySpeaker1", voice_dir="./voice_embeddings")
 
 print('Starting benchmark...')
 total_time_start = time.perf_counter()
 full_total = []
 folder = Path("./benchmark_audios")
 for wav_path in folder.glob("*.wav"):
-    audio = vc_model.new_load_audio(str(wav_path))
+    audio = vc_model.load_audio(str(wav_path))
     full_time = time.perf_counter()
-    converted_wav = vc_model.voice_conversion_preprocessed(audio,speaker_embedding)
+    converted_wav = vc_model.voice_conversion(audio, speaker_id="MySpeaker1", voice_dir="./voice_embeddings")
     full_time=time.perf_counter()-full_time
     full_total.append(full_time)
     print(f"generated conversion in ${full_time}")
